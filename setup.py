f02563cec3a1651c455a7823fffdfa63c39bfee8
Jacob Lurye <jlurye209@gmail.com>
2021-01-25 23:34:39 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lucier",
-    version="0.0.1",
+    version="0.0.2",
     author="Jacob Lurye",
     description="Sequence MIDI messages using a Flask-style interface.",
     long_description=long_description,
